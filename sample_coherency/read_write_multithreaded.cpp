e51eac925fca14cd5aad134bb3a10d8ca6f345cd
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2021-10-11 19:20:47 +0300

    Create read_write_multithreaded.cpp

---
+++ sample_coherency/read_write_multithreaded.cpp
@@ -0,0 +1,25 @@
+#include "DirectMappedMultiThreadCache.h"
+#include <map>
+#include <omp.h>
+
+int main(int argC, char ** argV)
+{
+		std::map<int,int> backingStore;
+
+    // single-level cache is inherently coherent with just using setThreadSafe getThreadSafe methods
+		DirectMappedMultiThreadCache<int,int> cache(10,
+				[&](int key){ return backingStore[key]; },
+				[&](int key, int value){ backingStore[key]=value; });
+
+		#pragma omp parallel for
+		for(int i=0;i<20;i++)
+		{
+			cache.setThreadSafe(i,i*2);
+		}
+
+		for(int i=0;i<20;i++)
+		{
+			std::cout<<cache.getThreadSafe(i)<<std::endl;
+		}
+		return 0;
+}
