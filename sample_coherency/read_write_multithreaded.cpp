1cc8f636c391fc950a96b494a2f77b9575e523df
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2021-10-11 19:23:01 +0300

    Update read_write_multithreaded.cpp

--- sample_coherency/read_write_multithreaded.cpp
+++ sample_coherency/read_write_multithreaded.cpp
@@ -5,22 +5,23 @@
 
 int main(int argC, char ** argV)
 {
-		std::map<int,int> backingStore;
+	std::map<int,int> backingStore;
 
-    // single-level cache is inherently coherent with just using setThreadSafe getThreadSafe methods
-		DirectMappedMultiThreadCache<int,int> cache(10,
-				[&](int key){ return backingStore[key]; },
-				[&](int key, int value){ backingStore[key]=value; });
+	// single-level cache is inherently coherent with just using setThreadSafe getThreadSafe methods
+	// direct mapped cache has low hit ratio. LruClockCache is better but it has only 1 synchronization point (whole LRU is locked)
+	DirectMappedMultiThreadCache<int,int> cache(10,
+			[&](int key){ return backingStore[key]; },
+			[&](int key, int value){ backingStore[key]=value; });
 
-		#pragma omp parallel for
-		for(int i=0;i<20;i++)
-		{
-			cache.setThreadSafe(i,i*2);
-		}
+	#pragma omp parallel for
+	for(int i=0;i<20;i++)
+	{
+		cache.setThreadSafe(i,i*2);
+	}
 
-		for(int i=0;i<20;i++)
-		{
-			std::cout<<cache.getThreadSafe(i)<<std::endl;
-		}
-		return 0;
+	for(int i=0;i<20;i++)
+	{
+		std::cout<<cache.getThreadSafe(i)<<std::endl;
+	}
+	return 0;
 }
