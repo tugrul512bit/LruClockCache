fd1a0d404caa8a6fb4efa960811c2a6060c09fbe
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2021-10-13 00:02:14 +0300

    Create direct_lru.cpp

---
+++ sample_single_thread_multi_level/direct_lru.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include"DirectMappedCache.h"
+#include"LruClockCache.h"
+
+int main()
+{
+
+  std::vector<char> backingStore(10000);
+
+  L2 is client of backing store
+  LruClockCache<size_t,char> L2(1000,
+    [&](size_t key){ return backingStore[key];},
+    [&](size_t key, char value){ backingStore[key]=value;}
+  );
+
+  // L1 is client of L2
+  DirectMappedCache<size_t,char> L1(100,
+    [&](size_t key){ return L2.get(key);},
+    [&](size_t key, char value){ L2.set(key,value);}
+  );
+ 
+  // use only L1
+  L1.set(9500,120);
+  std::cout<<L1.get(9500)<<std::endl;
+
+  // send awaiting bits of data to backing store
+  L1.flush();
+  L2.flush();
+
+  std::cout<<backingStore[9500]<<std::endl;
+  return 0;
+}
