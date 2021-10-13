a650b96b2c3ad12b84ebaad42a840d92719502fe
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2021-10-13 13:06:05 +0300

    Add files via upload

--- integer_key_specialization/NWaySetAssociativeMultiThreadCache.h
+++ integer_key_specialization/NWaySetAssociativeMultiThreadCache.h
@@ -16,8 +16,8 @@
 /* N parallel LRU approximations (Clock Second Chance)
 * Each with own mutex
 * cache-coherent writes+reads as long as user-given cache-miss functions handle the synchronization on the backing store
-* 				if you need also the backing-store be thread-safe, then put a single LRU cache behind this cache and use its getThreadSafe/setThreadSafe methods
-* 				but that cache will be bottlenecked by locking contention when this cache is thrashed frequently
+* 				each key is guarded by its own mutex guard so it shouldn't be a problem if backing-store can do parallel operations on different keys
+* 				if you need also the backing-store be thread-safe, then put a lock guard in the miss-functions
 * numberOfSets = number of LRUs in parallel (has to be power of 2: 2,4,8,...16k,32k,64k,....1M,2M,....)
 * numberOfTagsPerLRU = number of cache items per set (LRU Clock cache)
 * 			total size of cache is (numberOfSets * numberOfTagsPerLRU) elements
