baf5147faeda425f7067a4fd51aff1a1e895b5f9
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2021-10-25 15:08:12 +0300

    Update direct_3d.cpp

--- sample_multidimensional/direct_3d.cpp
+++ sample_multidimensional/direct_3d.cpp
@@ -3,19 +3,19 @@
 
 int main()
 {
- 		int backingStore[10][10][10];
-		DirectMapped3DMultiThreadCache<int,int> cache(4,4,4,
-				[&](int x, int y){ return backingStore[x][y]; },
-				[&](int x, int y, int value){  backingStore[x][y]=value; });
-		for(int i=0;i<10;i++)
-			for(int j=0;j<10;j++)
-        for(int k=0;k<10;k++)
-				cache.set(i,j,k,i+j+k); // depth-major
+	int backingStore[10][10][10];
+	DirectMapped3DMultiThreadCache<int,int> cache(4,4,4,
+			[&](int x, int y, int z){ return backingStore[x][y][z]; },
+			[&](int x, int y, int z, int value){  backingStore[x][y][z]=value; });
+	for(int i=0;i<10;i++)
+		for(int j=0;j<10;j++)
+			for(int k=0;k<10;k++)
+			cache.set(i,j,k,i+j+k);
 
-		cache.flush();
-		std::cout<<"-------------"<<std::endl;
+	cache.flush();
+	std::cout<<"-------------"<<std::endl;
 
-			for(int j=0;j<10;j++)for(int i=0;i<10;i++)
-				std::cout<<backingStore[i][j]<<std::endl;
-		return 0; 
+		for(int i=0;i<10;i++)for(int j=0;j<10;j++)for(int k=0;k<10;k++)
+			std::cout<<backingStore[i][j][k]<<std::endl;
+	return 0;
 }
