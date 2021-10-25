a772d8b121f254f43e7f7663c47876d8bd12f308
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2021-10-25 15:08:56 +0300

    Update direct_3d.cpp

--- sample_multidimensional/direct_3d.cpp
+++ sample_multidimensional/direct_3d.cpp
@@ -10,7 +10,7 @@ int main()
 	for(int i=0;i<10;i++)
 		for(int j=0;j<10;j++)
 			for(int k=0;k<10;k++)
-			cache.set(i,j,k,i+j+k);
+			cache.set(i,j,k,i+j+k); // depth major
 
 	cache.flush();
 	std::cout<<"-------------"<<std::endl;
