077f8f2d2d77eaa5abbf7732cb07c4d93acfe0f3
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2021-10-25 15:08:43 +0300

    Update direct_2d.cpp

--- sample_multidimensional/direct_2d.cpp
+++ sample_multidimensional/direct_2d.cpp
@@ -14,7 +14,7 @@ int main()
 		cache.flush();
 		std::cout<<"-------------"<<std::endl;
 
-			for(int j=0;j<10;j++)for(int i=0;i<10;i++)
-				std::cout<<backingStore[i][j]<<std::endl;
+		for(int i=0;i<10;i++)for(int j=0;j<10;j++)
+			std::cout<<backingStore[i][j]<<std::endl;
 		return 0; 
 }
