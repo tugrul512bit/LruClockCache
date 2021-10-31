0c38e2ff37dd8456a64362478364e6c9223127fb
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2021-10-31 13:48:53 +0300

    Update usage.cpp

--- sample_async/usage.cpp
+++ sample_async/usage.cpp
@@ -2,7 +2,7 @@
 
 /*
 std::vector<int> data(1000000);
-		AsyncCache<int,int> cache(128,128,1024,[&](int key){ return data[key]; },[&](int key, int value){ data[key]=value; });
+		AsyncCache<int,int> cache(128,1024,[&](int key){ return data[key]; },[&](int key, int value){ data[key]=value; });
 		int val;
 		int slot = cache.setAsync(5,100);
 		cache.getAsync(5,&val,slot);
