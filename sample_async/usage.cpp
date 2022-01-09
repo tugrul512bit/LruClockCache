cfe7dd09a1465e0adbaef529c6a83295edd2b28b
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2022-01-09 14:43:26 +0300

    Update usage.cpp

--- sample_async/usage.cpp
+++ sample_async/usage.cpp
@@ -4,7 +4,7 @@
 std::vector<int> data(1000000);
 		AsyncCache<int,int> cache(128,1024,[&](int key){ return data[key]; },[&](int key, int value){ data[key]=value; });
 		int val;
-		int slot = cache.setAsync(5,100);
+		int slot = cache.setAsync(5,100); // or this: int slot = omp_get_thread_num(); cache.setAsync(5,100,slot);
 		cache.getAsync(5,&val,slot);
 		std::cout<<data[5]<<" "<<val<<std::endl;
 		cache.barrier(slot);
